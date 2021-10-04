e4c06a25c058bc93f3797bb2fe1b049013b45c51
devephy <litephantus@gmail.com>
2021-10-05 00:52:29 +0300

    printf_function

--- printf.c
+++ printf.c
@@ -1,82 +1,103 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
+ * printTypeChecker - Checks for the characters(special) provided in the
+ * printf function
  *
- * Return: pointer to valid function or NULL
+ *  @car: Character that comes after the % symbol
+ *
+ *  @arg: Checker argument
+ *
+ *  Return: The number of characters needed
  */
-static int (*check_for_specifiers(const char *format))(va_list)
+
+int printTypeChecker(char car, va_list arg)
 {
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
+	int arrIndex;
+
+	TypeStruct argArr[] = {
+		{"c", ret_char},
+		{"s", ret_str},
 		{NULL, NULL}
 	};
 
-	for (i = 0; p[i].t != NULL; i++)
+	for (arrIndex = 0; argArr[arrIndex].t != NULL; arrIndex++)
 	{
-		if (*(p[i].t) == *format)
+		if (argArr[arrIndex].t[0] == car)
 		{
-			break;
+			return (argArr[arrIndex].out(arg));
 		}
 	}
-	return (p[i].f);
+
+	return (0);
 }
 
 /**
- * _printf - prints anything
- * @format: list of argument types passed to the function
+ * printf - Our custom printf function based on C's
  *
- * Return: number of characters printed
+ * @format : Character stream to be used
+ *
+ * Return: Characters or -1 when datatype does not exist
  */
+
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
+	unsigned int i;
+	int typeChecked = 0, charReturned = 0;
+	va_list arg;
+/* char *format(va_list); */
 
-	if (format == NULL)
-		return (-1);
-	va_start(valist, format);
-	while (format[i])
+	va_start(arg, format);
+
+/* if (format == NULL) */
+/* { */
+/* return (-1); */
+/* } */
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		for (; format[i] != '%' && format[i]; i++)
+		if (format[i] != '%')
 		{
 			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
+			charReturned++;
 			continue;
 		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		count++;
+
 		if (format[i + 1] == '%')
-			i += 2;
-		else
+		{
+			_putchar('%');
+			charReturned++;
 			i++;
+			continue;
+		}
+
+		if (format[i + 1] == '\0')
+		{
+			return (-1);
+		}
+
+		typeChecked = printTypeChecker(format[i + 1], arg);
+		
+		if (typeChecked == -1 || typeChecked != 0)
+		{
+			i++;
+		}
+
+		if (typeChecked > 0)
+		{
+			charReturned += typeChecked;
+		}
+
+		if (typeChecked == 0)
+		{
+			_putchar('%');
+			charReturned++;
+		}
+
 	}
-	va_end(valist);
-	return (count);
+
+	va_end(arg);
+	return (charReturned);
+
 }
